6eca1f3890c8e77a6a299b63525b59195e4e5c3c
IrinOrchi <irin.hoque.orchi@g.bracu.ac.bd>
2025-04-29 16:07:23 +0600

    accordion bootsrap to tailwind

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-accordion',
   standalone: true,
-  imports: [CommonModule, NgbAccordionModule],
+  imports: [CommonModule],
   templateUrl: './accordion.component.html',
   styleUrl: './accordion.component.scss',
 })
-export class AccordionComponent {}
+export class AccordionComponent {
+  isOpen = true;
+
+  toggleAccordion() {
+    this.isOpen = !this.isOpen;
+  }
+}
