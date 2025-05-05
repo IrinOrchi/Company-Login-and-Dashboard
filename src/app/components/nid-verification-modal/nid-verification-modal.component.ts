800c1d5cbe70fd7fdf3f8e57c640a1f5a8c2263b
IrinOrchi <irin.hoque.orchi@g.bracu.ac.bd>
2025-05-05 14:59:50 +0600

    nid verification modal

--- src/app/components/nid-verification-modal/nid-verification-modal.component.ts
+++ src/app/components/nid-verification-modal/nid-verification-modal.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalService } from '../../services/modal.service';
 
 @Component({
   selector: 'app-nid-verification-modal',
@@ -9,11 +9,24 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './nid-verification-modal.component.html',
   styleUrl: './nid-verification-modal.component.scss',
 })
-export class NidVerificationModalComponent {
-  constructor(public activeModal: NgbActiveModal) {}
+export class NidVerificationModalComponent implements OnInit, OnDestroy {
+  @Output() close = new EventEmitter<void>();
+  private readonly MODAL_ID = 'nid-verification-modal';
+  
+  constructor(private modalService: ModalService) {}
+
+  ngOnInit() {
+    document.body.style.overflow = 'hidden';
+    this.modalService.openModal(this.MODAL_ID);
+  }
+
+  ngOnDestroy() {
+    document.body.style.overflow = 'auto';
+    this.modalService.closeModal();
+  }
   
   dismiss() {
-    this.activeModal.dismiss('Cross click');
+    this.close.emit();
   }
 
   verifyNow() {
